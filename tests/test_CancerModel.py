55fe1c1c757db057e1f9e2a8bbff3e16b3e4f440
lrverdon <129669916+lrverdon@users.noreply.github.com>
2024-05-01 13:10:29 +0000

    Tests

---
+++ tests/test_CancerModel.py
@@ -0,0 +1,11 @@
+import unittest
+
+from cancer_prediction.cancer_model import CancerModel
+
+class TestCancerModel(unittest.TestCase):
+
+    def test_diagnosis_to_target(self):
+        model = CancerModel()
+        diagnosis = 'Malignant'
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target,0)
